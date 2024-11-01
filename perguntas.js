1d3c918b460d2579fc74615e8dfa039a8ee1bd12
Nicolyhigashi1 <161840485+Nicolyhigashi1@users.noreply.github.com>
2024-11-01 10:32:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é HARDWARE?',
+    'Os hardwares são as peças físicas que compõem um computador, como as placas, o monitor, o teclado, a placa-mãe e o disco rígido.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é SOFTWARE?    ',
+    'Software é uma coleção de programas e dados que dizem a um computador como executar tarefas específicas.'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual a fórmula para calcular a área de um triângulo?',
+    'A = (b x h)/2'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'Quais são os 4 pilares da educação financeira?',
+    'Reconhecer, registrar, revisar e realizar.'
+)
